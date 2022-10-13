e1c8d579c5ca18d0433b24e525171fcb9c4fbdfe
mmerzlyakov <mmerzlyakov@optisystems24.ru>
2022-10-13 14:23:49 +0300

    home_work_1

---
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -0,0 +1,12 @@
+package ru.otus;
+
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = Lists.newArrayList(1, 2, 3, 4);
+        System.out.println(integers);
+    }
+}
