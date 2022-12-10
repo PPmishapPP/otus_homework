6ecf399f7c920eaaf5204d36ea8581e52f3d2884
mmerzlyakov <mmerzlyakov@optisystems24.ru>
2022-12-10 11:24:00 +0300

    решение домашней работы hw16-io

--- hw16-io/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ hw16-io/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,14 +1,26 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+
+import java.io.FileWriter;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
 
+    private final String fileName;
+
     public FileSerializer(String fileName) {
+        this.fileName = fileName;
     }
 
     @Override
     public void serialize(Map<String, Double> data) {
-        //формирует результирующий json и сохраняет его в файл
+        Gson gson = new Gson();
+        String json = gson.toJson(data);
+        try (FileWriter fileWriter = new FileWriter(fileName)) {
+            fileWriter.write(json);
+        } catch (Exception ignore) {
+            //ignore
+        }
     }
 }
--- hw16-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw16-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,18 +1,36 @@
 package ru.otus.dataprocessor;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import ru.otus.model.Measurement;
 
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ResourcesFileLoader implements Loader {
 
+    private final String fileName;
 
     public ResourcesFileLoader(String fileName) {
+        this.fileName = fileName;
     }
 
     @Override
     public List<Measurement> load() {
-        //читает файл, парсит и возвращает результат
-        return null;
+        ClassLoader classLoader = getClass().getClassLoader();
+        Gson gson = new Gson();
+        try (Reader inputStream = new InputStreamReader(classLoader.getResourceAsStream(fileName))) {
+            Type type = new TypeToken<ArrayList<Measurement>>() {
+            }.getType();
+            return gson.fromJson(inputStream, type);
+        } catch (Exception ignore) {
+            //ignore
+        }
+
+        return Collections.emptyList();
     }
 }
