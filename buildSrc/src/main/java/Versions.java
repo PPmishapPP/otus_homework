397e838373f50415970c2f529f7bb263c43dff6e
mmerzlyakov <mmerzlyakov@optisystems24.ru>
2023-02-28 08:59:56 +0300

    Поправил версии

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -1,11 +1,11 @@
 public interface Versions {
-    String guava = "31.0.1-jre";
+    String guava = "31.1-jre";
     String jmh = "1.35";
     String asm = "9.2";
     String glassfishJson = "2.0.1";
 
     String lombok = "1.18.24";
-    String gson = "2.9.1";
+    String gson = "2.10.1";
 
     String mongodb = "4.7.1";
     String mongodbReactive = "4.7.1";
@@ -17,4 +17,14 @@ public interface Versions {
     String freemarker = "2.3.31";
 
     String reflections = "0.10.2";
+
+    String sockjs = "1.5.1";
+    String stomp = "2.3.4";
+    String bootstrap = "5.2.3";
+    String springDocOpenapiUi = "2.0.2";
+    String jsr305 = "3.0.2";
+
+    String errorProneAnnotations = "2.11.0";
+    String tomcatAnnotationsApi = "6.0.53";
+    String grpc = "1.44.1";
 }
--- hw21-jpql/src/main/java/ru/otus/crm/model/Phone.java
+++ hw21-jpql/src/main/java/ru/otus/crm/model/Phone.java
@@ -2,7 +2,7 @@ package ru.otus.crm.model;
 
 import lombok.*;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
